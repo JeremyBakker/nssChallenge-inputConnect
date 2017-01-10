d384b2c94927fd12a38539bc044af6efa8e5cdfa
Jeremy Bakker <jeremybakker@jeremys-mbp.attlocal.net>
2017-01-10 16:26:36 -0600

    added comments

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
+// This function takes information from the slider when it changes. It inserts that information into the number input.
 
 function showSlide(x) {
 	document.getElementById("digitalDisplay").value=x
 }
 
+
+// This function takes information from the number input onchange and onclick. It adjusts the slider with that information
+.
 function showTemp(x) {
 	document.getElementById("rangeDisplay").value=x
 }
-
